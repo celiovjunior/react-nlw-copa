cf2e6529c05a490ff55488d2660c12049837823a
Celio Junior <celio_cjr@hotmail.com>
2022-11-04 15:52:43 -0300

    initial commit

---
+++ server/prisma/seed.ts
@@ -0,0 +1,62 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+    const user = await prisma.user.create({
+        data: {
+            name: 'Maria',
+            email: 'maria@email.com',
+            avatarUrl: 'https://github.com/celiovjunior.png',
+        }
+    })
+
+    const pool = await prisma.pool.create({
+        data: {
+            title: 'example pool',
+            code: 'BOL123',
+            ownerId: user.id,
+
+            // O prisma permite fazer inserções encadeadas
+            participants: {
+                create: {
+                    userId: user.id
+                }
+            }
+        }
+    })
+
+    await prisma.game.create({
+        data: {
+            date: '2022-11-03T12:27:02.321Z',
+            firstTeamCountryCode: 'DE',
+            secondTeamCountryCode: 'BR'
+        }
+    })
+
+    await prisma.game.create({
+        data: {
+            date: '2022-11-04T14:00:00.321Z',
+            firstTeamCountryCode: 'BR',
+            secondTeamCountryCode: 'AR',
+
+            guesses: {
+                create: {
+                    firstTeamPoints: 2,
+                    secondTeamPoints: 1,
+
+                    participant: {
+                        connect: {
+                            userId_poolId: {
+                                userId: user.id,
+                                poolId: pool.id
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    })
+}
+
+main()
